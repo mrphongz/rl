f5b7e335f9f9f770d4c4bae663dfb516ba21788c
mrphongz <135221230+mrphongz@users.noreply.github.com>
2023-06-01 17:39:44 +0700

    roulette.js

--- roulette.js
+++ roulette.js
@@ -1,3 +1,16 @@
+// ==UserScript==
+// @name         rl
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.nimo.tv/fragments/act/roulette-game
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=nimo.tv
+// @grant        none
+// @verify       https://raw.githubusercontent.com/mrphongz/rl/main/roulette.js
+// ==/UserScript==
+
+
 const sleep = s => new Promise(r => setTimeout(r, s));
 const chips = document.querySelectorAll('.roulette-control-area__counter-item');
 function namMuoi(){
